01c3c901b385117a401aacdcedecdc0c61a44476
MTabakhian <tabakhianmohamad@gmail.com>
2024-09-01 22:57:20 +0330

    Add Pages

---
+++ Javascript/search-button.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      sidebarClose = document.querySelector(".sidebarClose");
+
+    //   Js code to toggle Search Box
+      searchToggle.addEventListener("click", () => {
+        searchToggle.classList.toggle("active");
+      });
+
+    // Js code to toggle sidebar 
+    sidebarOpen.addEventListener("click", () => {
+      nav.classList.add("active");
+    });
+
+    body.addEventListener("click", e => {
+      let clickedElm = e.target;
+
+      if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu-header")){
+        nav.classList.remove("active");
+      }
+    });
